c7da0773fd3e2d1dbac12ccee965ce7f286380cd
Himanshu7921 <himanshusr451tehs@gmail.com>
2026-05-27 19:18:59 +0530

    Included Adam optimzer related configurations

--- src/config.py
+++ src/config.py
@@ -5,10 +5,13 @@ config = {
     "out_features": 10,
     "hidden_dim": 16,
     "n_layers": 2,
-    "lr": 0.01,
+    "lr": 0.001,
     "alpha": 1e-3,
     "batch_size": 24,
     "epochs": 200,
     "dataset_path": "./dataset",
-    "save_path": "./saved_models"
+    "save_path": "./saved_models",
+    "beta1": 0.9,
+    "beta2": 0.999,
+    "eps": 1e-8
 }
