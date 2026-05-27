7ca3fc1c1f83894602d02a8a723938badd0289e4
Himanshu7921 <himanshusr451tehs@gmail.com>
2026-05-27 18:31:48 +0530

    experimenting with different configurations until i get a better convergence

---
+++ src/config.py
@@ -0,0 +1,14 @@
+config = {
+    "num_classes": 10,
+    "H": 28,
+    "W": 28,
+    "out_features": 10,
+    "hidden_dim": 16,
+    "n_layers": 2,
+    "lr": 0.01,
+    "alpha": 1e-3,
+    "batch_size": 24,
+    "epochs": 200,
+    "dataset_path": "./dataset",
+    "save_path": "./saved_models"
+}
